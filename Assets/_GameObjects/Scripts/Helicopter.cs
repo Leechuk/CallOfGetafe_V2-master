00a3922a30e1fee43efba89c15e3f3e4819a5304
Leechuk <55555794+Leechuk@users.noreply.github.com>
2019-10-03 13:12:25 +0200

    Update 03102019 Cambios
    Cambios

---
+++ Assets/_GameObjects/Scripts/Helicopter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour
+{
+    
+
+    [SerializeField] Transform target;
+    void Update()
+    {
+        /*Vector3 vt = new Vector3(0, Time.deltaTime, 0);
+        transform.Translate(vt);*/
+
+        //transform.Translate(Vector3.down * Time.deltaTime);
+
+
+        //Vector3.MoveTowards(transform.position, target.position,  Time.deltaTime);
+        //transform.LookAt(target);
+
+
+    }
+
+
+
+}
---
+++ Assets/_GameObjects/Scripts/SpawnerArea.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerArea : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefab; //Objeto que respawneamos
+    [SerializeField]
+    float delay; // tiempo que tardan en respawnear los enemigos
+    [SerializeField]
+    int limit; // Cantidad de objetos que Respawneamos
+    private int NumerosdeInstancias;
+    [SerializeField] Transform limiteSuperior;
+    [SerializeField] Transform limiteInferior;
+
+    void Start()
+    {
+        InvokeRepeating("Spawnear", 0, delay);
+    }
+
+    void Spawnear()
+    {
+        float x = Random.Range(limiteInferior.position.x, limiteSuperior.position.x);
+        float y = transform.position.y;
+        float z = Random.Range(limiteInferior.position.z, limiteSuperior.position.z);
+
+        // Instanciar el enemigo en la ubicacion del Spawner como hijo, pero si este se mueve se mueve detras los objetos.
+        //Instantiate(_prefab, transform);
+
+        // Instanciar el enemigo sin depender del Spawner
+        Instantiate(prefab, new Vector3(x, y, z), transform.rotation);
+        NumerosdeInstancias++; //Es lo mismo que numeroinstancias=numeroinstancias+1;
+
+        if (NumerosdeInstancias == limit) //Si llegamos al limite de enemigos se destruye el spawner
+        {
+            CancelInvoke();
+            //Destroy(this.gameObject); // se destruye este gameobject o Destroy(this); desactivamos el script
+        }
+
+    }
+}
